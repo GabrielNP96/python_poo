25d6f291f2eca2f67a28a0e30877d0d3fc378524
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-03 19:48:58 -0300

    declarando uma classe

---
+++ oo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,11 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = True
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes =[restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
