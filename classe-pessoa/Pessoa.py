c2bc8b8abb2283d06889a52d2d48ca3a734fb70a
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-04 14:23:28 -0300

    classe pessoa

---
+++ classe-pessoa/Pessoa.py
@@ -0,0 +1,20 @@
+class Pessoa:
+    pessoas = []
+    def __init__(self,nome, idade, profissao):
+        self._nome = nome
+        self._idade = idade
+        self._profissao = profissao
+        Pessoa.pessoas.append(self)
+
+    def __str__(self):
+        print(f'{self._nome.ljust(25)} | {self._idade.ljust(25)} | {self._profissao}' )
+    
+    def aniversario(self):
+        self._idade = + 1
+    
+    @property
+    def saudacao(self):
+        print(f'Me chamo {self._nome} e trabalho como {self._profissao}')
+
+    
+
