a71a5f1fd64c86ad2121dd4e6de9a69836ba5abf
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-04 12:58:27 -0300

    refatorando

--- classe-musica/musica.py
+++ classe-musica/musica.py
@@ -1,25 +1,19 @@
-class musica:
-    nome = ''
-    artista = ''
-    duracao = 0
+class Musica:
 
-imagine_dragons_beliver = musica()
-imagine_dragons_beliver.nome = 'Belviver'
-imagine_dragons_beliver.artista = 'Imagine Dragons'
-imagine_dragons_beliver.duracao = 3.37
-print(vars(imagine_dragons_beliver))
-print('-' * 40)
+    musicas = []
 
-twenty_one_pilots_stressed_out= musica()
-twenty_one_pilots_stressed_out.nome = 'Stressed out'
-twenty_one_pilots_stressed_out.artista = 'Twenty one Pilots'
-twenty_one_pilots_stressed_out.duracao = 3.46
-print(vars(twenty_one_pilots_stressed_out))
-print('-' * 40)
+    def __init__(self, nome, artista, duracao):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+        Musica.musicas.append(self)
+    
+    def listar_musicas():
+        for musicas in Musica.musicas:
+            print(f'{musicas.nome} | {musicas.artista} | {musicas.duracao}')
 
-artic_monkeys_arabela = musica()
-artic_monkeys_arabela.nome = 'Arabela'
-artic_monkeys_arabela.artista = 'Artic Monkeys'
-artic_monkeys_arabela.duracao = 4.11
-print(vars(artic_monkeys_arabela))
-print('-' * 40)
+imagine_dragons_beliver = Musica('Beliver', 'Imagine Dragons', 3.37)
+twenty_one_pilots_stressed_out= Musica('Stressed out', 'Twenty one Pilots', 3.46)
+artic_monkeys_arabela = Musica('Arabela', 'Artic Monkeys', 4.11)
+
+Musica.listar_musicas()
