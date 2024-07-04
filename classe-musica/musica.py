46814cc60b856e0c960eb36d40a09446455c6968
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-03 23:55:48 -0300

    instaciando uma classe

---
+++ classe-musica/musica.py
@@ -0,0 +1,25 @@
+class musica:
+    nome = ''
+    artista = ''
+    duracao = 0
+
+imagine_dragons_beliver = musica()
+imagine_dragons_beliver.nome = 'Belviver'
+imagine_dragons_beliver.artista = 'Imagine Dragons'
+imagine_dragons_beliver.duracao = 3.37
+print(vars(imagine_dragons_beliver))
+print('-' * 40)
+
+twenty_one_pilots_stressed_out= musica()
+twenty_one_pilots_stressed_out.nome = 'Stressed out'
+twenty_one_pilots_stressed_out.artista = 'Twenty one Pilots'
+twenty_one_pilots_stressed_out.duracao = 3.46
+print(vars(twenty_one_pilots_stressed_out))
+print('-' * 40)
+
+artic_monkeys_arabela = musica()
+artic_monkeys_arabela.nome = 'Arabela'
+artic_monkeys_arabela.artista = 'Artic Monkeys'
+artic_monkeys_arabela.duracao = 4.11
+print(vars(artic_monkeys_arabela))
+print('-' * 40)
